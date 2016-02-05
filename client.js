ad41b22a5e62abbae103a91e0f56967cdf512c98
WEBER Antoine <ghostsmaker@hotmail.com>
2016-02-05 20:40:34 +0100

    gestion des images envoyer depuis le client

--- client.js
+++ client.js
@@ -1,7 +1,11 @@
-var io = require('socket.io-client')('http://192.168.0.24:7000');
+//var io = require('socket.io-client')('http://localhost:7000');
 //var socket = require('socket.io');
 var fs = require('fs');
 var ss = require('socket.io-stream');
+
+var io = require('socket.io-client');
+var socket = io.connect('http://localhost:7000');
+var folder = "./client_local_data/";
 
 // Fonction qui liste les noms de fichiers, précédés du chemin fourni et les retourne dans une variable fileList.
 function getFileList(dirName, callback) {
@@ -11,41 +15,47 @@ function getFileList(dirName, callback) {
     });
 }
 
-// La variable path est le deuxième argument fournit dans le processus
-var path = process.argv[2];
+// La variable user_id est le deuxième argument fournit dans le processus
+var user_id = process.argv[2];
+
+if(!user_id){
+  console.log('merci de preciser votre nom de user en argument, exemple: node client.js test_user');
+  exit();//devrais etre une fonction de process, mais j'ai pas le net... alors je laisse planter
+}
+
+socket.emit("identity",{
+  user_id: user_id,
+  user_type: "node_client"
+});
+
 // La variable printList est une fonction anonyme qui console.log l'argument fourni.
 var printList = function(liste) {
     console.log(liste);
 }
 // La variable sendList est une fonction anonyme qui envoie la liste récupérée via socket.io.
 var sendList = function(liste) {
-    console.log("j'ai envoyé liste");
+    console.log("j'ai envoyé ma liste de fichier");
     console.log(liste);
-    io.emit('list', liste);
+    socket.emit('list', {
+      list: liste,
+      user: user_id
+    });
 }
 
-// Discussion avec le serveur distant :
-// Quand Socket.IO reçoit un message de type 'getList', il déclenche une fonction qui a pour paramètre le message envoyé par le serveur, inutile ici. A la place, on récupère la liste.
-//io.on('connection', function(socket){
-
-  io.emit('list', ['test']);
-//});
-
-console.log("j'ai envoyé liste test");
-
-  io.on('getList',function(){
-    console.log("j'ai recu getLIst");
-    // On déclenche la fonction getFileList avec la variable path comme argument dirName et la fonction contenue dans la variable sendList comme argument callback.
-    getFileList(path,function(listeA) {
-        sendList(listeA);
-    });
+socket.on('getList',function(){
+  console.log("j'ai recu getList");
+  // On déclenche la fonction getFileList avec la variable path comme argument dirName et la fonction contenue dans la variable sendList comme argument callback.
+  getFileList(folder,function(listeA) {
+      sendList(listeA);
   });
+});
 
-//});
+socket.on('serverLoadImage',function(path){
+  console.log("j'ai recu serverLoadImage");
+  var filename = folder+path;
+  var stream = ss.createStream();
+  ss(socket).emit('sendImageToServer', stream, {user: user_id,name: path});
+  fs.createReadStream(filename).pipe(stream);
+});
 
-// On déclenche la fonction getFileList avec la variable path comme argument dirName et la fonction contenue dans la variable printList comme argument callback.
-//getFileList(path,printList);
-
-
-// On créé un stream.
-//var stream = ss.createStream();
+console.log("en attente de requete");
--- index.js
+++ index.js
@@ -1,26 +1,90 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var ss = require('socket.io-stream');
 var path = require('path');
+var fs = require('fs');
 var port = 7000;
 
+var tmp_folder = "./tmp_server/";
+
+function createDirOrUse(path,cb){
+  fs.mkdir(path,function(e){
+    if(!e || (e && e.code === 'EEXIST')){
+        //do something with contents
+        cb();
+    } else {
+        //debug
+        console.log(e);
+    }
+  });
+}
+
+//partie socket io pour echanger avec les clients
 io.on('connection', function(socket){
   console.log('user connexion');
+  var user_id = null;
+  var user_type = "web_client";
+
+  socket.on('identity',function(data){
+    console.log("reception d'une identité");
+    console.log(data);
+    var user_id = data.user_id;
+    var user_type = data.user_type;
+  })
+
+
   //retransmission des messages
   socket.on('list', function(list){
     console.log("j'ai retransmis liste");
     io.emit('list', list);
   });
 
-  socket.on('getList', function(list){
-    console.log("j'ai retransmis getList");
-    io.emit('getList', list);
+  socket.on('getList', function(user){
+    console.log("j'ai demande getList à chaque user");
+    io.emit('getList', user);
   });
+
+  socket.on('serverLoadImage', function(path){
+    console.log("j'ai retransmis serverLoadImage");
+    io.emit('serverLoadImage', path);
+  });
+
+  ss(socket).on('sendImageToServer', function(stream, data) {
+    console.log('sendImageToServer');
+    var user = data.user;
+
+    var dirname = path.join(__dirname,tmp_folder,user);
+    var filename = path.join(dirname,data.name);
+
+    createDirOrUse(dirname,function(){
+      stream.pipe(fs.createWriteStream(filename, {autoClose: true}));
+      io.emit('serverHasLoadedImage', {
+        user: user,
+        path: data.name
+      });
+    });
+
+  });
+
 });
 
-
+//Partie express pour servir les data web
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname,'index.html'));
+});
+
+//servir les images
+app.get('/image/:user_id/:filename', function (req, res, next) {
+  var user = req.params.user_id;
+  var filename = req.params.filename;
+  var filepath = path.join(__dirname,tmp_folder,user,filename);
+  res.contentType('image/jpeg');//filepath);
+  res.sendFile(filepath);
+});
+
+app.get('/lib/:file',function(req,res,next){
+  res.sendFile(path.join(__dirname,'lib',req.params.file));
 });
 
 http.listen(port , function(){
